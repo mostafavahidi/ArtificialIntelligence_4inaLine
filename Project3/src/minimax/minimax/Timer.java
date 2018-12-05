a2f6b6fd84a8c5ea91b9ac29d488f5bbd441db77
Alec Strickland <alecjstrickland@gmail.com>
2018-12-04 23:19:50 -0800

    Removed annoying output

--- Project3/src/minimax/minimax/Timer.java
+++ Project3/src/minimax/minimax/Timer.java
@@ -11,10 +11,9 @@ public class Timer implements Runnable {
 	public void run() {
 		final long SECONDS_25 = 25000;
 		try {
-			System.out.println("Timer is going to sleep for 25 seconds...");
 			Thread.sleep(SECONDS_25);
 			parentThread.interrupt();
-			System.out.println("Time is up, parent thread interrupted!");
+			System.out.println("Time is up, search has been interruted!");
 		} catch (InterruptedException e) {
 			// The timer has been canceled
 		}
